43bf9999bf54e4ca46aca68250da1e9cfbfc6b19
FAIZAN.KHAZI <faizankhazi8@gmail.com>
2024-05-17 05:51:21 +0530

    Updates

---
+++ precache-manifest.2396d912c9e3e9354276b038bb3f603f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3520465b5c9bbe0dbc4f70895c49f0dd",
+    "url": "/Textutilx-React/index.html"
+  },
+  {
+    "revision": "2001a7c1827ddf7b25ac",
+    "url": "/Textutilx-React/static/css/main.e970e176.chunk.css"
+  },
+  {
+    "revision": "1a66c8ab625c1a11d327",
+    "url": "/Textutilx-React/static/js/2.c220b0d7.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/Textutilx-React/static/js/2.c220b0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319bfefe5a0e23db7d0a",
+    "url": "/Textutilx-React/static/js/3.ed8653ad.chunk.js"
+  },
+  {
+    "revision": "2001a7c1827ddf7b25ac",
+    "url": "/Textutilx-React/static/js/main.8eef17c3.chunk.js"
+  },
+  {
+    "revision": "5947e3f077c20228c29e",
+    "url": "/Textutilx-React/static/js/runtime-main.2b4b4017.js"
+  }
+]);
